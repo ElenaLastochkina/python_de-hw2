4d9301257ac7c5e6bf256cc17e7ece2d314f33a8
Elena <elastochkina@mail.ru>
2023-09-24 22:16:45 +0300

    2/2

--- hw2.py
+++ hw2.py
@@ -2,9 +2,12 @@
 # его шестнадцатеричное строковое представление. 
 # Функцию hex используйте для проверки своего результата.
 
-decimal_number = int(input("введите целое число: "))
-hex_number = hex(decimal_number)
-print("шестнадцатеричное представление числа:", hex_number)
+def int_to_hex(number):
+    return hex(number)
+
+input_number = int(input("введите целое число: "))
+hex_string = hex(input_number)
+print("шестнадцатеричное представление числа с помощью функции hex:", hex_string)
 
 # 3. Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем. 
 # Программа должна возвращать сумму и произведение* дробей. 
@@ -12,14 +15,30 @@ print("шестнадцатеричное представление числа:
 
 from fractions import Fraction
 
-fraction1_str = input("Введите первую дробь (вида а/b): ")
-fraction2_str = input("Введите вторую дробь (вида а/b): ")
+def calculate_fraction(fraction1, fraction2):
+    fraction1 = Fraction(fraction1)
+    fraction2 = Fraction(fraction2)
 
-fraction1 = Fraction(fraction1_str)
-fraction2 = Fraction(fraction2_str)
+    sum_fraction = fraction1 + fraction2
+    product_fraction = fraction1 * fraction2
+
+    return sum_fraction, product_fraction
+
+input_fraction1 = input("Введите первую дробь (вида а/b): ")
+input_fraction2 = input("Введите вторую дробь (вида а/b): ")
+
+sum_fraction, product_fraction = calculate_fraction(input_fraction1, input_fraction2)
+print("Сумма двух дробей:", sum_fraction)
+print("Произведение двух дробей:", product_fraction)
+
+
+from fractions import Fraction
+
+fraction1 = Fraction("1/2")
+fraction2 = Fraction("3/4")
 
 sum_fraction = fraction1 + fraction2
 product_fraction = fraction1 * fraction2
 
-print("Сумма двух дробей:", sum_fraction)
-print("Произведение двух дробей:", product_fraction)
+print("Сумма дробей с помощью модуля fractions:", sum_fraction)
+print("Произведение дробей с помощью модуля fractions:", product_fraction)
