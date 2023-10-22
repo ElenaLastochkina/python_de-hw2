3085a435b00fb769a5788266129f7367a675221d
Elena <elastochkina@mail.ru>
2023-10-22 19:13:33 +0300

    2/2

--- hw2.py
+++ hw2.py
@@ -2,43 +2,58 @@
 # его шестнадцатеричное строковое представление. 
 # Функцию hex используйте для проверки своего результата.
 
-def int_to_hex(number):
-    return hex(number)
+HEX = 16
+hex_digits = "0123456789ABCDEF"
 
-input_number = int(input("введите целое число: "))
-hex_string = hex(input_number)
-print("шестнадцатеричное представление числа с помощью функции hex:", hex_string)
+hex_num = ""
+test_hex_num = hex(num)
+
+while num > 0:
+    remainder = num % HEX
+    hex_num = hex_digits[remainder] + hex_num
+    num //= HEX
+
+print("Шестнадцатеричное представление числа:", hex_num)
+print("Проверка результата:", test_hex_num)
+
 
 # 3. Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем. 
 # Программа должна возвращать сумму и произведение* дробей. 
 # Для проверки своего кода используйте модуль fractions
 
 from fractions import Fraction
+frac1 = '2/5'
+frac2 = '3/5'
 
-def calculate_fraction(fraction1, fraction2):
-    fraction1 = Fraction(fraction1)
-    fraction2 = Fraction(fraction2)
+# Разбиваем строки на числитель и знаменатель без использования map
+numerator1_str, denominator1_str = frac1.split('/')
+numerator2_str, denominator2_str = frac2.split('/')
 
-    sum_fraction = fraction1 + fraction2
-    product_fraction = fraction1 * fraction2
+# Преобразуем строки в целые числа
+numerator1 = int(numerator1_str)
+denominator1 = int(denominator1_str)
+numerator2 = int(numerator2_str)
+denominator2 = int(denominator2_str)
 
-    return sum_fraction, product_fraction
+common_denominator = denominator1 * denominator2
 
-input_fraction1 = input("Введите первую дробь (вида а/b): ")
-input_fraction2 = input("Введите вторую дробь (вида а/b): ")
+new_numerator1 = numerator1 * denominator2
+new_numerator2 = numerator2 * denominator1
 
-sum_fraction, product_fraction = calculate_fraction(input_fraction1, input_fraction2)
-print("Сумма двух дробей:", sum_fraction)
-print("Произведение двух дробей:", product_fraction)
+summation_numerator = new_numerator1 + new_numerator2
+multiplication_numerator = numerator1 * numerator2
 
+print(f"Сумма дробей: {summation_numerator}/{common_denominator}")
+print(f"Произведение дробей: {multiplication_numerator}/{common_denominator}")
 
-from fractions import Fraction
+# Преобразуем введенные строки в объекты Fraction
+fraction1 = Fraction(frac1)
+fraction2 = Fraction(frac2)
 
-fraction1 = Fraction("1/2")
-fraction2 = Fraction("3/4")
+# Вычисляем сумму и произведение дробей
+summation = fraction1 + fraction2
+multiplication = fraction1 * fraction2
 
-sum_fraction = fraction1 + fraction2
-product_fraction = fraction1 * fraction2
+print(f"Сумма дробей: {summation}")
+print(f"Произведение дробей: {multiplication}")
 
-print("Сумма дробей с помощью модуля fractions:", sum_fraction)
-print("Произведение дробей с помощью модуля fractions:", product_fraction)
